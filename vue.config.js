a0d62be9e3aea6df1d723d2791532b0d030bc43b
Linus Bartsch <git@libasoft.de>
2021-11-17 02:59:02 +0100

    add pwa support

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    pwa: {
+        name: 'KIT Campus Tracker',
+        themeColor: '#009682',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        manifestOptions: {
+            short_name: 'CampusTracker',
+            start_url: '/',
+            background_color: '#ffffff',
+            display: 'standalone',
+            description: 'A utility to simplify Covid-19 related contact tracing on the KIT Campus. Creates a list of your used rooms.',
+        },
+    },
+}
